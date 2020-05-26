99b04ea3c7984e7225e7c6092f049057856863af
Johannes Siipola <johannes.siipola@creuna.fi>
2020-05-26 14:25:12 +0300

    Fix autoload

--- vips-image-editor.php
+++ vips-image-editor.php
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
 
 // Check if we are using local Composer
 if (file_exists(__DIR__ . '/vendor')) {
-    require 'vendor/autoload.php';
+    require __DIR__ . '/vendor/autoload.php';
 }
 
 include 'class-image-editor-vips.php';
